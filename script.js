2705e506e7d0c8b6fcfd9044d01799feb90a58fd
W.Carlos <wendcarlos18@hotmail.com>
2021-06-07 16:13:02 -0300

    const incomeDisplay
    Exibindo o conteúdo no DOM

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const transactionsUl = document.querySelector('#transactions')
+const incomeDisplay = document.querySelector('#money-plus')
+const expenseDisplay = document.querySelector('#money-minus')
+const balanceDisplay = document.querySelector('#balance')
 
 const dummyTransactions = [
     {id: 1, name: 'Bolo de brigadeiro', amount: -20},
@@ -34,7 +37,11 @@ const updateBalanceValues = () => {
     const expense = transactionsAmounts.filter(value => value < 0)
         .reduce((accumulator, value) => accumulator + value, 0)
         .toFixed(2)
-    console.log(expense)
+    
+    balanceDisplay.textContent = `R$ ${total}`
+    incomeDisplay.textContent = `R$ ${income}`
+    expenseDisplay.textContent = `R$ ${expense}`
+
 }
 
 /*Quando a página for carregada vai inserir, a init vai adicionar as transações no DOM */
