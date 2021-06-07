5a15a0ec4252d50bb86ad5037ef0f9e44d372bb1
W.Carlos <wendcarlos18@hotmail.com>
2021-06-07 16:02:21 -0300

    transactionsAmounts

--- script.js
+++ script.js
@@ -22,8 +22,19 @@ const addTransactionIntoDOM = transaction => {
 }
 
 const updateBalanceValues = () => {
-    const transactionsAmounts = dummyTransactions.map(transaction => transaction.amount)
-    console.log(transactionsAmounts)
+    const transactionsAmounts = dummyTransactions
+        .map(transaction => transaction.amount)
+    const total = transactionsAmounts
+        .reduce((accumulator, transaction) => accumulator + transaction, 0)
+        .toFixed(2)
+    const income = transactionsAmounts
+        .filter(value => value > 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    const expense = transactionsAmounts.filter(value => value < 0)
+        .reduce((accumulator, value) => accumulator + value, 0)
+        .toFixed(2)
+    console.log(expense)
 }
 
 /*Quando a página for carregada vai inserir, a init vai adicionar as transações no DOM */
